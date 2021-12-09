c113f3d1123e80cba913cb2fd4fd4b3523bea9e2
Jacky <964413011@qq.com>
2021-12-09 21:26:55 +0800

    更新打包脚本

--- script.py
+++ script.py
@@ -1,28 +1,42 @@
-from genericpath import exists
+import tarfile
+import shutil
 import os
+import logging
 
-os.makedirs("./sqls",exist_ok=True)
+logging.info("building START")
 
-files=[os.path.join("./migrations",x) for x in os.listdir("./migrations")]
+with os.popen("cargo build --release") as info:
+    while True:
+        s = info.readline()
+        if s == "":
+            break
+        logging.info(s)
 
-with open("data.sql",'w')as sql:
+logging.info("building DONE")
+
+logging.info("conbin sql START")
+os.makedirs("./sqls", exist_ok=True)
+
+files = [os.path.join("./migrations", x) for x in os.listdir("./migrations")]
+
+with open("./sql/data.sql", 'w')as sql:
     for fn in files:
-        with open(fn,"r")as src:
-            sql.write( src.read())
+        with open(fn, "r")as src:
+            sql.write(src.read())
             sql.write("\n-- next table \n")
 
-print("conbin sql DONE")
+logging.info("conbin sql DONE")
 
-os.makedirs("./pack",exist_ok=True)
+os.makedirs("./pack", exist_ok=True)
 
-import shutil
+print("package Files START")
 
-shutil.copyfile("./sqls/data.sql","./pack/db.sql")
-shutil.copyfile("./target/release/illustrators_support", "./pack/illustrators_support")
+shutil.copyfile("./sqls/data.sql", "./pack/db.sql")
+shutil.copyfile("./target/release/illustrators_support",
+                "./pack/illustrators_support")
 
-import tarfile
 
-file=tarfile.open("./pack.tar","w:gz")
+file = tarfile.open("./pack.tar", "w:gz")
 file.add("./pack/db.sql")
 file.add("./pack/illustrators_support")
 
