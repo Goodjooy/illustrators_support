21755e0d4a186e62351e3ac4086a205b2503f246
ForzenString <964413011@qq.com>
2021-12-10 20:04:40 +0800

    简化脚本

--- script.py
+++ script.py
@@ -2,13 +2,6 @@ import tarfile
 import shutil
 import os
 
-print("building START")
-
-with os.popen("cargo build --release") as info:
-    pass
-
-print("building DONE")
-
 print("conbin sql START")
 os.makedirs("./sqls", exist_ok=True)
 
@@ -22,19 +15,4 @@ with open("./sqls/data.sql", 'w')as sql:
 
 print("conbin sql DONE")
 
-os.makedirs("./pack", exist_ok=True)
 
-print("package Files START")
-
-shutil.copyfile("./sqls/data.sql", "./pack/db.sql")
-shutil.copyfile("./target/release/illustrators_support",
-                "./pack/illustrators_support")
-
-
-file = tarfile.open("./pack.tar", "w:gz")
-file.add("./pack/db.sql")
-file.add("./pack/illustrators_support")
-
-file.close()
-
-print("package Files DONE")
